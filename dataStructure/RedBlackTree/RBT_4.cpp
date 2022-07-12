b7c92d0b3988e6ff849d4afa3ef7b3ab3263bbf5
orioripaori <100837224+orioripaori@users.noreply.github.com>
2022-07-12 15:06:50 +0900

    레드블랙트리 한번더 도전

--- dataStructure/RedBlackTree/RBT_4.cpp
+++ dataStructure/RedBlackTree/RBT_4.cpp
@@ -1,1 +1,55 @@
 //abcd
+#include<iostream>
+using namespace std;
+class node{
+    private:
+    int data;
+    int color;//black 0 red 1
+    node *parent;
+    node *left, *right;
+    public:
+    node(int number, int colour){
+        data=number;
+        color=colour;
+    }
+    ~node(){
+        delete this;
+    }
+    void setData(int number){
+        data=number;
+    }
+    void setColor(int colour){
+        color=colour;
+    }
+    void getData(){
+        cout<<data<<"\n";
+    }
+    void getColor(){
+        cout<<color<<"\n";
+    }
+    void setParent(node *something){
+        parent=something;
+    }
+    void setLeft(node *something){
+        left=something;
+    }
+    void setRight(node *something){
+        right=something;
+    }
+    node* insertion(node *head, node *newOne){
+
+    }
+    node* deletion(node *head, node *target){
+
+    }
+    void inorderPrint(node *head){//이거는 일단 된거같음
+        if(head!=NULL){
+            inorderPrint(head->left);
+            head->getData();
+            inorderPrint(head->right);
+        }
+    }
+    void find(node *head, node* target){
+
+    }
+};

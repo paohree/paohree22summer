78439073e7bc02f23a64fa711cf99bc606de03cb
orioripaori <100837224+orioripaori@users.noreply.github.com>
2022-08-01 00:06:03 +0900

    다했다

--- baekjoon/week5_7/programers_12914.cpp
+++ baekjoon/week5_7/programers_12914.cpp
@@ -1,1 +1,22 @@
 //멀리뛰기
+#include<iostream>
+using namespace std;
+int main(void){
+    int number=0;
+    int answer=0;
+    cin>>number;
+    if(number==1){
+        cout<<number;
+    }else if(number==2){
+        cout<<number;
+    }else{
+        int number1=0;
+        int number2=1;
+        for(int i=1;i<=number;++i){
+            answer=number1+number2;
+            number1=number2;
+            number2=answer;
+        }
+        cout<<answer;
+    }
+}
